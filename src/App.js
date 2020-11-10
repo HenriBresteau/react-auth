9bdbf1fa5119012955b60895285e426fbab4a592
henriB <henri.bretseau@gmail.com>
2020-11-10 11:55:01 +0100

    ui config

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import firebase from './utils/firebaseConfig';
+import Main from './component/Main';
+import { StyledFirebaseAuth } from 'react-firebaseui';
 
-function App() {
+const App = () => {
+
+  const [isSignedIn, setSignedIn] = useState(false);
+
+  const uiConfig = {
+    signInFlow: 'popup',
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    ],
+    callbacks: {
+      signInSuccess: () => false,
+    }
+  };
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      setSignedIn(!!user);
+
+    })
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app' style={{ textAlign: 'center' }}>
+      { isSignedIn ? (
+        <Main />
+        ) : (
+          <div className="login-page">
+            <h1>React Crud</h1>
+            <StyledFirebaseAuth
+              uiConfig={uiConfig}
+            firebaseAuth={firebase.auth()}
+            />
+          </div>
+        )}
     </div>
   );
-}
+};
 
 export default App;

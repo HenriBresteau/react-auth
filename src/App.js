ce50e3a66843032783ca49457befbb591ca689e2
henriB <henri.bretseau@gmail.com>
2020-11-11 17:12:29 +0100

    uiIDcontext

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import firebase from './utils/firebaseConfig';
-import Main from './component/Main';
-import { StyledFirebaseAuth } from 'react-firebaseui';
+import React, { useEffect, useState } from "react";
+import firebase from "./utils/firebaseConfig";
+import Main from "./component/Main";
+import { StyledFirebaseAuth } from "react-firebaseui";
+import { UiIdContext } from "./component/UiidContext";
 
 const App = () => {
-
   const [isSignedIn, setSignedIn] = useState(false);
+  const [uid, setUid] = useState(null);
 
   const uiConfig = {
-    signInFlow: 'popup',
+    signInFlow: "popup",
     signInOptions: [
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
       firebase.auth.FacebookAuthProvider.PROVIDER_ID,
@@ -16,30 +17,32 @@ const App = () => {
     ],
     callbacks: {
       signInSuccess: () => false,
-    }
+    },
   };
 
   useEffect(() => {
     firebase.auth().onAuthStateChanged((user) => {
       setSignedIn(!!user);
-
-    })
+      setUid(user.uid);
+    });
   }, []);
 
   return (
-    <div className='app' style={{ textAlign: 'center' }}>
-      { isSignedIn ? (
-        <Main />
+    <UiIdContext.Provider value={uid}>
+      <div className="app" style={{ textAlign: "center" }}>
+        {isSignedIn ? (
+          <Main />
         ) : (
           <div className="login-page">
             <h1>React Crud</h1>
             <StyledFirebaseAuth
               uiConfig={uiConfig}
-            firebaseAuth={firebase.auth()}
+              firebaseAuth={firebase.auth()}
             />
           </div>
         )}
-    </div>
+      </div>
+    </UiIdContext.Provider>
   );
 };
 

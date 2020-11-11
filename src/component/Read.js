46f43d1c3700657a53db16c38fab600860b8191b
henriB <henri.bretseau@gmail.com>
2020-11-11 12:46:51 +0100

    read d'apres firebase database

--- src/component/Read.js
+++ src/component/Read.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import firebase from '../utils/firebaseConfig';
 
 const Read = () => {
+
+    const [quoteList,setQuoteList] = useState([]);
+
+    useEffect( ()=>{
+        const quoteDB = firebase.database().ref('quotesDB');
+        quoteDB.on('value', (snapshot)=>{
+            console.log(snapshot.val());
+            let previousList = snapshot.val();
+            let list =[];
+            for (let id in previousList){
+                list.push({id, ...previousList[id]})
+            }
+            setQuoteList(list);
+        })
+    },[]);
+
     return (
-        <div>
-            
+        <div className='read'>
+            <ul>
+                { quoteList && quoteList.map((item) =>{
+                    return <p>{ item.text} from {item.author}</p>
+                })}
+            </ul>
         </div>
     );
 };

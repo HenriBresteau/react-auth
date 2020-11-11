042d99a195c1511c494de53ac639b8f3ed85a7e2
henriB <henri.bretseau@gmail.com>
2020-11-11 17:03:15 +0100

    Update

--- src/component/Read.js
+++ src/component/Read.js
@@ -1,33 +1,33 @@
-import React, { useEffect, useState } from 'react';
-import firebase from '../utils/firebaseConfig';
-import UpdateDelete from './UpdateDelete';
+import React, { useEffect, useState } from "react";
+import firebase from "../utils/firebaseConfig";
+import UpdateDelete from "./UpdateDelete";
 
 const Read = () => {
+  const [quoteList, setQuoteList] = useState([]);
 
-    const [quoteList,setQuoteList] = useState([]);
+  useEffect(() => {
+    const quoteDB = firebase.database().ref("quotesDB");
+    quoteDB.on("value", (snapshot) => {
+      console.log(snapshot.val());
+      let previousList = snapshot.val();
+      let list = [];
+      for (let id in previousList) {
+        list.push({ id, ...previousList[id] });
+      }
+      setQuoteList(list);
+    });
+  }, []);
 
-    useEffect( ()=>{
-        const quoteDB = firebase.database().ref('quotesDB');
-        quoteDB.on('value', (snapshot)=>{
-            console.log(snapshot.val());
-            let previousList = snapshot.val();
-            let list =[];
-            for (let id in previousList){
-                list.push({id, ...previousList[id]})
-            }
-            setQuoteList(list);
-        })
-    },[]);
-
-    return (
-        <div className='read'>
-            <ul>
-                { quoteList && quoteList.map((item,index) =>(
-                    <UpdateDelete item={item} key={index} />
-                ))}
-            </ul>
-        </div>
-    );
+  return (
+    <div className="read">
+      <ul>
+        {quoteList &&
+          quoteList.map((item, index) => (
+            <UpdateDelete item={item} key={index} />
+          ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Read;
--- src/component/UpdateDelete.js
+++ src/component/UpdateDelete.js
@@ -1,12 +1,55 @@
-import React from 'react';
-import firebase from '../utils/firebaseConfig';
+import React, { useState } from "react";
+import firebase from "../utils/firebaseConfig";
 
-const UpdateDelete = () => {
-    return (
-        <div>
-            
+const UpdateDelete = ({ item }) => {
+  const [update, setUpdate] = useState(false);
+  const [authorUpdate, setAuthorUpdate] = useState(null);
+  const [textUpdate, setTextUpdate] = useState(null);
+
+  const updateItem = () => {
+    let quote = firebase.database().ref("quoteDB").child(item.id);
+    if (authorUpdate !== null) {
+      quote.update({
+        author: authorUpdate,
+      });
+    }
+    if (textUpdate !== null) {
+      quote.update({
+        text: textUpdate,
+      });
+    }
+    setUpdate(false);
+  };
+
+  return (
+    <li>
+      {update === false && (
+        <div className="item-container">
+          <p>"{item.text}"</p>
+          <h6>{item.author}</h6>
+          <div className="buttons-container">
+            <button onClick={() => setUpdate(!update)}>Update</button>
+            <button>Delete</button>
+          </div>
         </div>
-    );
+      )}
+
+      {update && (
+          <div className="item-container-update">
+                <textarea 
+                    defaultValue= {item.text}
+                    onChange = {(e)=>setTextUpdate(e.target.value)}
+                ></textarea>
+                <input 
+                    type="text"
+                    defaultValue={item.author}
+                    onChange={(e)=> setAuthorUpdate(e.target.value)}
+                />
+                <button onClick={updateItem}>Validate Update</button>
+          </div>
+      )}
+    </li>
+  );
 };
 
 export default UpdateDelete;

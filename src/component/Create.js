80baed30cc2f43d65249f264434ec659e7010843
henriB <henri.bretseau@gmail.com>
2020-11-11 11:53:02 +0100

    lien avec firebase database + Display

--- src/component/Create.js
+++ src/component/Create.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import firebase from '../utils/firebaseConfig';
 
 const Create = () => {
+    const [author, setAuthor] = useState('');
+    const [text, setText] = useState('');
+
+    const createQuote = () => {
+        const quoteDB = firebase.database().ref("quoteDB")
+        const quote = {
+            author,
+            text,
+        };
+        quoteDB.push(quote);
+        setAuthor('');
+        setText('');
+    }
     return (
-        <div>
-            
+        <div className='create'>
+            <h4>Déposer une citation</h4>
+            <div className="form">
+                <input
+                    type="text"
+                    placeholder="Auteur"
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                />
+                <textarea
+                    placeholder='Citation'
+                    value={text}
+                    onChange={(e) => setText(e.target.value)}
+                ></textarea>
+                <button onClick={createQuote}>Create</button>
+            </div>
+
         </div>
     );
 };
